de5d05cc85a6fa99a153941b3c4a7881f271fca4
Alex <mr.portmane@gmail.com>
2019-07-15 14:25:57 -0700

    comments for firstHibernateProgram v - 0.0.1

--- src/main/java/frameworks/hibernate/firstHibernateProgram/firstHibernateProgram.java
+++ src/main/java/frameworks/hibernate/firstHibernateProgram/firstHibernateProgram.java
@@ -13,22 +13,26 @@ import java.util.List;
 
 
 public class firstHibernateProgram {
-    private static SessionFactory factory;                                      //Help elements.
-    private static firstHibernateProgram FHP = new firstHibernateProgram();     //Help elements.
+    private static SessionFactory factory;                                      /*Variable for storage of factory of Hiber-
+                                                                                * nate sessionFactory instance.*/
+    private static firstHibernateProgram FHP = new firstHibernateProgram();     /*Instance of class for free use of
+                                                                                * static methods inside of main method
+                                                                                * of the class(firstHibernateProgram).*/
     public static void main(String[] args) {
 
         try {
-            factory = new Configuration().configure().buildSessionFactory();
+            factory = new Configuration().configure().buildSessionFactory();    //Receiving of this sessionFactory instance.
         } catch (Throwable ex) {
             System.err.println("Failed to create sessionFactory object." + ex);
-            throw new ExceptionInInitializerError(ex);
+            throw new ExceptionInInitializerError(ex);                          //NOT NOW !!!
         }
 
         //Add a few employee records in database.
         Integer empId1 = FHP.addEmployee("Zara", "133", "Zara123@mail.ru");
         Integer empId2 = FHP.addEmployee("Dara", "HelloWorld", "Emplo@mail.ru");
         Integer empId3 = FHP.addEmployee("Zara", "133", "Zara123@resurch.ru");
-        Integer emplId4 = FHP.addEmployee("Pole", "OleTheBest5", "work@unnamed.com", "Vincent", "Churchill");
+        Integer emplId4 = FHP.addEmployee("Pole", "OleTheBest5", "work@unnamed.com",
+                "Vincent", "Churchill");
 
         //List down all the employees.
         FHP.listEmployees();
@@ -45,22 +49,24 @@ public class firstHibernateProgram {
 
     //Method to CREATE an employee in the database.
     private Integer addEmployee(String username, String password, String user_email) {
-        Transaction tx = null;
-        Integer employeeId = null;
-
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            Employee employee = new Employee(username, password, user_email);
-            employeeId = (Integer) session.save(employee);
-            tx.commit();
+        Transaction tx = null;                                                  /*Variable for storing of current tran-
+                                                                                * saction with database.*/
+        Integer employeeId = null;                                              /*Variable for storing the index of employee.*/
+        try (Session session = factory.openSession()) {                         //Try-witch resources.
+            tx = session.beginTransaction();                                    //Essential receiving of current transaction.
+            Employee employee = new Employee(username, password, user_email);   //Creation the employee.
+            employeeId = (Integer) session.save(employee);                      //Saving and getting employee ID.
+            tx.commit();                                                        /*Commit changes in database. Caused by
+                                                                                * this transaction.*/
         } catch (HibernateException exc) {
             if (tx != null)
-                tx.rollback();
-            exc.printStackTrace();
+                tx.rollback();                                                  //NOT NOW !!!
+            exc.printStackTrace();                                              //NOT NOW !!!
         }
-        return employeeId;
+        return employeeId;                                                      //Returning Employee ID.
     }
-    //Overloaded for 2-nd constructor.
+    /* Overloaded for 2-nd constructor.
+     Structure comments are the same as in the overloaded method.                !!!!!!!!!!!!!!!!!!!!!!!!*/
     private Integer addEmployee(String username, String password, String user_email, String first_name, String last_name) {
         Transaction tx = null;
         Integer employeeId = null;
@@ -80,54 +86,73 @@ public class firstHibernateProgram {
 
     //Method to DELETE an employee from the records.
     private void deleteEmployee(Integer EmployeeId) {
-        Transaction tx = null;
+        Transaction tx = null;                                                  //Variable for storing of the current transaction.
 
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            Employee employee = (Employee) session.get(Employee.class, EmployeeId);
-            session.delete(employee);
-            tx.commit();
+        try (Session session = factory.openSession()) {                         //Try-with resources statement.
+            tx = session.beginTransaction();                                    //Opening the transaction.
+            Employee employee = (Employee) session.get(Employee.class, EmployeeId); /*Receiving the employee from database
+                                                                                    * by class of the row and primary key.*/
+            session.delete(employee);                                           /*Essential deleting of the employee in
+                                                                                * current session.*/
+            tx.commit();                                                        //Commit the changes.
         } catch (HibernateException exc) {
-            if (tx != null) tx.rollback();
-            exc.printStackTrace();
+            if (tx != null) tx.rollback();                                      //NOT NOW !!!
+            exc.printStackTrace();                                              //NOT NOW !!!
         }
     }
 
     //Method to UPDATE username for an employee.
     private void updateEmployee(Integer EmployeeId, String Username) {
-        Transaction tx = null;
+        Transaction tx = null;                                                  //Variable for storing of the current transaction.
 
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            Employee employee = (Employee) session.get(Employee.class, EmployeeId);
-            employee.setUsername(Username);
-            session.update(employee);
-            tx.commit();
+        try (Session session = factory.openSession()) {                         //Try-with resources statement.
+            tx = session.beginTransaction();                                    //Opening of the transaction.
+            Employee employee = (Employee) session.get(Employee.class, EmployeeId); /*Receiving the employee from database
+                                                                                    * by class of the row and primary key.*/
+            employee.setUsername(Username);                                     /*Changing the employee information through
+                                                                                * get (and set) method(s).*/
+            session.update(employee);                                           //Pulling changes to database.
+            tx.commit();                                                        //Commit the changes.
         } catch (HibernateException exc) {
-            if (tx != null) tx.rollback();
-            exc.printStackTrace();
+            if (tx != null) tx.rollback();                                      //NOT NOW !!!
+            exc.printStackTrace();                                              //NOT NOW !!!
         }
     }
 
     //Method to READ all the employees.
     private void listEmployees() {
-        Transaction tx = null;
+        Transaction tx = null;                                                  //Variable for storing of the current transaction.
 
-        try (Session session = factory.openSession()) {
-            tx = session.beginTransaction();
-            List employees = session.createQuery("FROM Employee").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext(); ) {
-                Employee employee = (Employee) iterator.next();
-                System.out.println("Username: " + employee.getUsername());
+        try (Session session = factory.openSession()) {                         //Try-with resources statement.
+            tx = session.beginTransaction();                                    //Opening of the transaction.
+            List employees = session.createQuery("FROM Employee").list();    /*Creating the query from Employee class
+                                                                                * rows and converting it ti the list witch
+                                                                                * will be assigned to employees variable.*/
+            for (Iterator iterator = employees.iterator(); iterator.hasNext(); ) {  /*For loop witch is using the Iterator
+                                                                                    * object for representation of employees
+                                                                                    * in the table. Much more simple solu-
+                                                                                    * tion represented and commented below.*/
+                Employee employee = (Employee) iterator.next();                 //Getting next Employee in the list of the rows.
+                System.out.println("Username: " + employee.getUsername());      //Representation of them through get methods.
                 System.out.println("    Password: " + employee.getPassword());
                 System.out.println("        User email: " + employee.getUser_email());
                 System.out.println("            First name: " + employee.getFirst_name());
                 System.out.println("                Second name: " + employee.getLast_name());
             }
-            tx.commit();
+            /*for (Object emp:                                                    //For-each version.
+                 employees) {
+                Employee employee = (Employee) emp;
+                System.out.println("Username: " + employee.getUsername());       //Representation of them through get methods.
+                System.out.println("    Password: " + employee.getPassword());
+                System.out.println("        User email: " + employee.getUser_email());
+                System.out.println("            First name: " + employee.getFirst_name());
+                System.out.println("                Second name: " + employee.getLast_name());
+            }
+            */
+            tx.commit();                                                        //Commit changes.
         } catch (HibernateException exc) {
-            if (tx != null) tx.rollback();
-            exc.printStackTrace();
+            if (tx != null) tx.rollback();                                      //NOT NOW !!!
+            exc.printStackTrace();                                              //NOT NOW !!!
         }
     }
 }
