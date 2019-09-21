24f235373e7af7bee062555fad11b49825c3a438
Alex <mr.portmane@gmail.com>
2019-09-21 13:20:21 -0700

    CourseApiApp.java v - 0.0.1   <<- "Spring Boot" starter, doesn't work with this configuration ->>

---
+++ src/main/java/frameworks/spring/springBoot/tasks/WILLBERENAMEDLATER/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,12 @@
+package frameworks.spring.springBoot.tasks.WILLBERENAMEDLATER.io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
