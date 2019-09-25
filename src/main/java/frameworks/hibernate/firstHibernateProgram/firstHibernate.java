faec2b236d2da88105fb4268f907cf2037f9103b
Alex <mr.portmane@gmail.com>
2019-09-25 10:49:49 -0700

    firstHibernate.md v - 0.0.4 <<- COMMENTS ->>

--- src/main/java/frameworks/hibernate/firstHibernateProgram/firstHibernate.java
+++ src/main/java/frameworks/hibernate/firstHibernateProgram/firstHibernate.java
@@ -11,7 +11,7 @@ public class firstHibernate {
                                                             /*Creat EntityManagerFactory variable with use of name
                                                             of the persistence-unit in persistence.xml.*/
 
-        Employee emp1 = new Employee();                     /*creating and modifying a variable.*/
+        Employee emp1 = new Employee();                     /*Then we create and modify the variable.*/
         emp1.setUsername("Paul");
         emp1.setPassword("1234567777");
         emp1.setUser_email("firstDayofthework@gmail.com");
@@ -21,7 +21,7 @@ public class firstHibernate {
                                                             which will be used as controller for future changes in data-
                                                             base.*/
         entityManager.getTransaction().begin();             /*Gets the current transaction and stat it.*/
-        entityManager.persist(emp1);                        /*Modify the future commit.*/
+        entityManager.persist(emp1);                        /*Modifies the future commit.*/
         entityManager.getTransaction().commit();            /*Gets the transaction and commit all changes made during
                                                             the thread process.*/
 
