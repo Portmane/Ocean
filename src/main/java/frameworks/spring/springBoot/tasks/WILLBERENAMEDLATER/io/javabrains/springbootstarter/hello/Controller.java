90f9216f9d685dac91115a3fe5a6a3e6370e61b4
Alex <mr.portmane@gmail.com>
2019-09-25 14:10:05 -0700

    Controller.java v - 0.0.1 <<- Controller for "/hello" ->>

---
+++ src/main/java/frameworks/spring/springBoot/tasks/WILLBERENAMEDLATER/io/javabrains/springbootstarter/hello/Controller.java
@@ -0,0 +1,14 @@
+package frameworks.spring.springBoot.tasks.WILLBERENAMEDLATER.io.javabrains.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/hello")
+    public String sayHi() {
+        return "Hi";
+    }
+}
