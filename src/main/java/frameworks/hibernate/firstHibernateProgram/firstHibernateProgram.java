e9126aeee9bf82b6f6a9d57765a8e57e8a6c56fc
Alex <mr.portmane@gmail.com>
2019-09-26 13:55:17 -0700

    firstHibernate.java v - 0.0.5 <<- RENAME ->>

--- src/main/java/frameworks/hibernate/firstHibernateProgram/firstHibernateProgram.java
+++ src/main/java/frameworks/hibernate/firstHibernateProgram/firstHibernateProgram.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 
-public class firstHibernate {
+public class firstHibernateProgram {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("org.hibernate.firstHibernateProgram", null);
                                                             /*Creat EntityManagerFactory variable with use of name
