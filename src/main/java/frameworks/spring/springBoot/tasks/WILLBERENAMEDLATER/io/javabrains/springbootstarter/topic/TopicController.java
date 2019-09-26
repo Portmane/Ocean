941d30124dcd24800f161a542b4159ae7176285f
Alex <mr.portmane@gmail.com>
2019-09-26 13:42:21 -0700

    TopicController.java v - 0.0.2 <<- Adding control with use of service ->>

--- src/main/java/frameworks/spring/springBoot/tasks/WILLBERENAMEDLATER/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/frameworks/spring/springBoot/tasks/WILLBERENAMEDLATER/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,20 +1,21 @@
 package frameworks.spring.springBoot.tasks.WILLBERENAMEDLATER.io.javabrains.springbootstarter.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Java", "Core Java Description"),
-                new Topic("javascript", "JavaScript", "JavaScript Description"));
+        return topicService.getTopicList();
     }
 }
