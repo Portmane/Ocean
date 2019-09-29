6ed691ec5bfd257c21498cef683e39f25a1d4214
Alex <mr.portmane@gmail.com>
2019-09-29 11:40:02 -0700

    Topic.java v - 0.1.1 <<- DATA "Migration" ->>

--- src/main/java/frameworks/spring/springBoot/tasks/springBootCapabilities/io/javabrains/springbootstarter/topic/Topic.java
+++ src/main/java/frameworks/spring/springBoot/tasks/springBootCapabilities/io/javabrains/springbootstarter/topic/Topic.java
@@ -1,4 +1,4 @@
-package frameworks.spring.springBoot.tasks.WILLBERENAMEDLATER.io.javabrains.springbootstarter.topic;
+package frameworks.spring.springBoot.tasks.springBootCapabilities.io.javabrains.springbootstarter.topic;
 
 public class Topic {
 
