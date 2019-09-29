c6b965bf634649252a71330115292641e3726252
Alex <mr.portmane@gmail.com>
2019-09-29 11:40:52 -0700

    TopicController.java v - 0.1.1 <<- Adds "post", "update" request methods "Migration" ->>

--- src/main/java/frameworks/spring/springBoot/tasks/springBootCapabilities/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/frameworks/spring/springBoot/tasks/springBootCapabilities/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,4 +1,4 @@
-package frameworks.spring.springBoot.tasks.WILLBERENAMEDLATER.io.javabrains.springbootstarter.topic;
+package frameworks.spring.springBoot.tasks.springBootCapabilities.io.javabrains.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
