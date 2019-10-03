bcd2c6683ff14691869b8640eadf288f106a3afe
Alex <mr.portmane@gmail.com>
2019-10-03 13:38:12 -0700

    hashMapGetSetsAndCollections.java v - 1.0.0 <<- Module: "JavaClasses", Commit: "Ways of obtaining different Sets or Collection of values of the specific Map entry." ->>

--- JavaClasses/src/main/java/mapInterface/hashMap/hashMapGetSetsAndCollections.java
+++ JavaClasses/src/main/java/mapInterface/hashMap/hashMapGetSetsAndCollections.java
@@ -1,11 +1,9 @@
-package javaClasses.mapInterface.hashMap;
-
+package mapInterface.hashMap;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
 
 
 public class hashMapGetSetsAndCollections {
@@ -18,15 +16,15 @@ public class hashMapGetSetsAndCollections {
         countryISOCodeMapping.put("China", "CN");
 
 
-        //HashMap's entry set.
+        //HashMaps entry set.
         Set<Map.Entry<String, String>> countryISOCodeEntries = countryISOCodeMapping.entrySet();
         System.out.println("countryISOCode entries: " + countryISOCodeEntries);
 
-        //HashMap's key set.
+        //HashMaps key set.
         Set<String> countries = countryISOCodeMapping.keySet();
         System.out.println("Countries: " + countries);
 
-        //HashMap's values.
+        //HashMaps values.
         Collection<String> iscCodes = countryISOCodeMapping.values();
         System.out.println("isoCodes: " + iscCodes);
     }
