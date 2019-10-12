7485f9cfadcb06cf53235a1e6f49da151c63bbff
Alex <mr.portmane@gmail.com>
2019-10-12 13:14:04 -0700

    UserService.java v - 0.2.0 <<- Module: "SpringBootDataJpaApp", Comment: "Service for Spring Boot APP." ->>

--- Frameworks/Spring/SpringBoot/SpringBootDataJpaApp/src/main/java/io/pages/user/UserService.java
+++ Frameworks/Spring/SpringBoot/SpringBootDataJpaApp/src/main/java/io/pages/user/UserService.java
@@ -1,10 +1,9 @@
-package io.user;
+package io.pages.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
