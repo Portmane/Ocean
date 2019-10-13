b7902e25c182aafd7fdd0777ef5515fa1679f27e
Alex <mr.portmane@gmail.com>
2019-10-13 10:35:22 -0700

    UserRepository.java v - 1.0.0 <<- Module: "SpringBootDataJpaApp", Comment: "Creating the method based on "group" variable added to the "User.java" file for mapping." ->>

--- Frameworks/Spring/SpringBoot/SpringBootDataJpaApp/src/main/java/io/pages/user/UserRepository.java
+++ Frameworks/Spring/SpringBoot/SpringBootDataJpaApp/src/main/java/io/pages/user/UserRepository.java
@@ -2,6 +2,9 @@ package io.pages.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 
 public interface UserRepository extends CrudRepository<User, String> {
+    public List<User> findByGroupId(String groupId );
 }
