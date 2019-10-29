0eac9187b179e702cdee8c10c8f36a8572f4f4bd
Alex <mr.portmane@gmail.com>
2019-10-29 15:12:14 -0700

    firstHibernateProgram.java v - 1.0.3 <<- Comment: "Demo solution of the project issue: "FirstHibernateProgram module update #51".", Module: "FirstHibernateProgram" ->>

--- Frameworks/Hibernate/FirstHibernateProgram/src/main/java/firstHibernateProgram.java
+++ Frameworks/Hibernate/FirstHibernateProgram/src/main/java/firstHibernateProgram.java
@@ -1,7 +1,6 @@
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
 
 public class firstHibernateProgram {
@@ -10,36 +9,42 @@ public class firstHibernateProgram {
                                                             /*Creat EntityManagerFactory variable with use of name
                                                             of the persistence-unit in persistence.xml.*/
 
-        Employee emp1 = new Employee();                     /*Then we create and modify the variable.*/
+        Employee emp1 = new Employee();                     /*Then we are creating and modifying variables.*/
         emp1.setUsername("Paul");
         emp1.setPassword("1234567777");
         emp1.setPassword("999");
         emp1.setUser_email("firstDayofthework@gmail.com");
-        Employee emp2 = new Employee();                     /*Then we create and modify the variable.*/
+        Employee emp2 = new Employee();
         emp2.setUsername("Paul123");
         emp2.setPassword("1234567777123");
         emp2.setPassword("999123");
+        emp2.setFirst_name("It is not real.");
         emp2.setUser_email("firstDayofthework@gmail.com123");
 
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();   /*Creates EntityManager(session)
                                                             which will be used as controller for future changes in data-
                                                             base.*/
-        Query query = entityManager.createNativeQuery("SELECT COUNT(a.id) from users a");
-        Object minimalIdValue = query.getSingleResult();
-        //Integer minimalIdValue = entityManager.createQuery("SELECT COUNT(\" id \") FROM users").executeUpdate();
-        //(Integer) entityManager.SQLqu("SELECT COUNT(`id`) FROM users").uniqu
+        Object minimalIdValue =  entityManager.createNativeQuery("SELECT COUNT(a.id) from users a").getSingleResult();
+                                                            /*Query which counts number of existing users before commit
+                                                            * in the transaction.*/
+        System.out.println(minimalIdValue);                 //This line displays the result before commit.
 
-        System.out.println(minimalIdValue);
+
         entityManager.getTransaction().begin();             /*Gets the current transaction and stat it.*/
         entityManager.persist(emp1);                        /*Modifies the future commit.*/
         entityManager.persist(emp2);                        /*Modifies the future commit.*/
         entityManager.getTransaction().commit();            /*Gets the transaction and commit all changes made during
                                                             the thread process.*/
-        Query query2 = entityManager.createNativeQuery("SELECT COUNT(a.id) from users a");
-        Object minimalIdValue2 = query2.getSingleResult();
-        System.out.println(minimalIdValue2);
 
-        entityManager.close();                              /*Close the EntityManager(session).*/
+
+
+        Object minimalIdValue2 = entityManager.createNativeQuery("SELECT COUNT(a.id) from users a").getSingleResult();
+                                                            /*Query which counts number of existing users after commit
+                                                             * in the transaction.*/
+        System.out.println(minimalIdValue2);                //This line displays the result after commit.
+
+
+        entityManager.close();                              /*Closes the EntityManager(session).*/
     }
 }
