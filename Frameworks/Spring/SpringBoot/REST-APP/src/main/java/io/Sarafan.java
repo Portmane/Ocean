6ee87cc6952c0d1aaa11420f2ba7649fb22e5315
Alex <mr.portmane@gmail.com>
2019-11-03 12:05:44 -0800

    Sarafan v - 0.0.1 <<- Comment: "Starter for REST-APP.", Module: "REST-APP" ->>

---
+++ Frameworks/Spring/SpringBoot/REST-APP/src/main/java/io/Sarafan.java
@@ -0,0 +1,12 @@
+package io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Sarafan {
+    public static void main(String[] args) {
+        SpringApplication.run(Sarafan.class, args);
+    }
+}
