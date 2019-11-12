64edf1b369028b578bceb7eb8fa8cab5ba1e18c4
Alex <mr.portmane@gmail.com>
2019-11-12 14:06:28 -0800

    main.js v - 0.0.5 <<- Comment: "Edit button.", ERROR: "Have a mistake. Need to be FIXED.", Module: "REST-APP" ->>

--- Frameworks/Spring/SpringBoot/REST-APP/src/main/resources/static/js/main.js
+++ Frameworks/Spring/SpringBoot/REST-APP/src/main/resources/static/js/main.js
@@ -1,13 +1,30 @@
+function getIndexFromListById(listOfSmth, id) {
+    for (var i = 0; i < listOfSmth.length; i++) {
+        if (listOfSmth[i] === id) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 var databaseData = Vue.resource('/messages{/id}');
 
 
 Vue.component('messageForm', {
-    props: ['messages'],
-    data: function() {
-            return {
-                text: ''
-            }
-        },
+    props: ['messages', 'objOfEditableMessage'],
+    data: function () {
+        return {
+            text: '',
+            id: ''
+        }
+    },
+    watch: {
+        objOfEditableMessage: function (newVal, oldVal) {
+            this.text = newVal.text;
+            this.id = newVal.id;
+        }
+    },
     template: '<div>' +
         '<input type="text" placeholder="Write smth" v-model = "text" />' +
         '<input type="button" value="Save" @click = "save"/>' +
@@ -16,20 +33,39 @@ Vue.component('messageForm', {
         save: function () {
             var message = {text: this.text};
 
-            databaseData.save({}, message).then(result =>
-                result.json().then(data => {
-                        this.messages.push(data);
-                        this.text = "";
+            if (this.id) {
+                databaseData.update({id: this.id}, message).then(result =>
+                    result.json().then(data => {
+                        var indexInListWitchWillBeChanged = getIndexFromListById(this.messages, data.id);
+                        this.messages.splice(indexInListWitchWillBeChanged, 1, data);
+
+
+                        this.id = '';
+                        this.message = '';
                     }));
+            } else {
+                databaseData.save({}, message).then(result =>
+                    result.json().then(data => {
+                        this.messages.push(data);
+
+
+                        this.text = '';
+                    }));
+            }
         }
-        
+
     }
 });
 Vue.component('listOfMessages', {
     props: ['messages'],
-    template:   '<div>' +
-        '<messageForm :messages = "messages" />' +
-        '<messageRow v-for = "message in messages" :key = "message.id" :message = "message"></messageRow>' +
+    data: function () {
+        return {
+            message: null
+        }
+    },
+    template: '<div>' +
+        '<messageForm :messages = "messages" :objOfEditableMessage = "message" />' +
+        '<messageRow v-for = "message in messages" :key = "message.id" :message = "message" :editMethod = "editMethod"></messageRow>' +
         '</div>',
     created: function () {
         databaseData.get().then(result =>
@@ -38,35 +74,32 @@ Vue.component('listOfMessages', {
                     this.messages.push(message))));
     },
     methods: {
-        editMessage: function () {
-
+        editMethod: function (message) {
+            this.message = message;
         }
     }
 
 });
 Vue.component('messageRow', {
-    props: ['message'],
-    template:   '<div>' +
-                    '<i>({{  message.id  }})</i>   {{  message.text  }}' +
-                        '<span>' +
-                            '<input type="button" value="Edit" @click = "edit" />' +
-                        '</span>' +
-                '</div>',
+    props: ['message', 'editMethod'],
+    template: '<div>' +
+        '<i>({{  message.id  }})</i>   {{  message.text  }}' +
+        '<span>' +
+        '<input type="button" value="Edit" @click = "edit" />' +
+        '</span>' +
+        '</div>',
     methods: {
         edit: function () {
-
+            this.editMethod(this.message);
         }
     }
 });
-
 
 
 var app = new Vue({
     el: '#app',
     template: '<listOfMessages :messages = "messages" />',
     data: {
-        messages: [
-
-        ]
+        messages: []
     }
 });
