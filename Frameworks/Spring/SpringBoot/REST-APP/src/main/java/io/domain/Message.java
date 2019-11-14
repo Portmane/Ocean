6739a6fa50d46af03c3b706c64c961f47ae3bb3c
Alex <mr.portmane@gmail.com>
2019-11-14 08:55:52 -0800

    Message.java v - 0.0.1 <<- Comment: "Data of the REST-APP.", Module: "REST-APP_RAW" ->>

---
+++ Frameworks/Spring/SpringBoot/REST-APP/src/main/java/io/domain/Message.java
@@ -0,0 +1,35 @@
+package io.domain;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@ToString(of = {"id", "text"})
+@EqualsAndHashCode(of = {"id"})
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String text;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
