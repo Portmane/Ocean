1aa4529c0274071c676d57aa0c7e0b5e46181765
Alex <mr.portmane@gmail.com>
2019-11-18 10:13:20 -0800

    Message.java v - 0.0.3 <<- Comment: "Adding DATE field to the Message entity.", Module: "REST-APP" ->>

--- Frameworks/Spring/SpringBoot/REST-APP/src/main/java/io/domain/Message.java
+++ Frameworks/Spring/SpringBoot/REST-APP/src/main/java/io/domain/Message.java
@@ -1,10 +1,13 @@
 package io.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "message")
@@ -13,9 +16,29 @@ import javax.persistence.*;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView(Views.Id.class)
     private long id;
+
+
+
+    @Column(name = "text")
+    @JsonView(Views.IdAndText.class)
     private String text;
 
+    @Column(updatable = false, name = "creation_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime creationDate;
+
+
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
 
     public long getId() {
         return id;
