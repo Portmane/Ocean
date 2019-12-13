3a3d7ea659fe4946d2f340a04844fc47bf84ca02
Alex <mr.portmane@gmail.com>
2019-12-13 14:03:31 -0800

    MessageController.java v - 0.0.3 <<- Comment: "Controller in our APP.", Module: "Sweater" ->>

--- Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/controller/MessageController.java
+++ Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/controller/MessageController.java
@@ -1,17 +1,29 @@
 package io.controller;
 
+import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
+import io.domain.Message;
+import io.repos.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class MessageController {
+    @Autowired                                          // Give us
+    private MessageRepository messageRepository;        // Variable which store MessageRepository interface instance.
+
+
+
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("name", "Hello me!!!");
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
         return "mustache/index";
     }
 
@@ -21,5 +33,21 @@ public class MessageController {
                         Map<String, Object> model) {
         model.put("name", name);
         return "mustache/main";
+    }
+
+
+
+    @PostMapping
+    public String add(@RequestParam String text,@RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+
+        messageRepository.save(message);
+
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+
+
+        return "mustache/index";
     }
 }
