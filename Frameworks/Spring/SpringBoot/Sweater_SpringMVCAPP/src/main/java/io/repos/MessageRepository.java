64620559f128094a4077fb61f16432bd617acd50
Alex <mr.portmane@gmail.com>
2019-12-15 06:55:27 -0800

    MessageRepository.java v - 0.0.2 <<- Comment: "New method for "filterByTag" method in controller.", Module: "Sweater" ->>

--- Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/repos/MessageRepository.java
+++ Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/repos/MessageRepository.java
@@ -3,6 +3,8 @@ package io.repos;
 import io.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MessageRepository extends CrudRepository<Message, Integer> {   // Stores all instances of (Message class/message table).
+import java.util.List;
 
+public interface MessageRepository extends CrudRepository<Message, Integer> {   // Stores all instances of (Message class/message table).
+    List<Message> findByTag(String tag);                // Finding Messages by tag parameter.
 }
