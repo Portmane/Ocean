6d5ef5b9bc2e4a4b72f45dd3bb254833388c7951
Alex <mr.portmane@gmail.com>
2019-12-15 06:54:32 -0800

    MessageController.java v - 0.0.4 <<- Comment: "Controller in our APP. Standard methods.", Module: "Sweater" ->>

--- Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/controller/MessageController.java
+++ Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/controller/MessageController.java
@@ -1,6 +1,5 @@
 package io.controller;
 
-import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
 import io.domain.Message;
 import io.repos.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,39 +12,58 @@ import java.util.Map;
 
 @Controller
 public class MessageController {
-    @Autowired                                          // Give us
+    @Autowired                                          // Give us instance(singleton) of the bean.
     private MessageRepository messageRepository;        // Variable which store MessageRepository interface instance.
 
 
 
 
-    @GetMapping
+    @GetMapping                                         // Standard mapping for GET method.
     public String main(Map<String, Object> model) {
-        Iterable<Message> messages = messageRepository.findAll();
+        Iterable<Message> messages = messageRepository.findAll();   // Returns all messages existed in repository.
 
-        model.put("messages", messages);
+        model.put("messages", messages);                            // Updating of the model.
+
+
         return "mustache/index";
     }
 
 
-    @GetMapping("/hello")
+    @GetMapping("/hello")                               // Get mapping to "/hello" path.
     public String hello(@RequestParam(name = "name", required = false, defaultValue = "WORLD") String name,
                         Map<String, Object> model) {
-        model.put("name", name);
+        model.put("name", name);                        // Updating of the model.
+
+
         return "mustache/main";
     }
 
 
 
-    @PostMapping
-    public String add(@RequestParam String text,@RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    @PostMapping                                        // Standard POST mapping.
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);       // Creation of the new instance(Entity).
 
-        messageRepository.save(message);
+        messageRepository.save(message);                // Saving the new instance.
+
+        Iterable<Message> messages = messageRepository.findAll();   // Getting all messages.
+        model.put("messages", messages);                            // Updating of the model.
 
 
-        Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+        return "mustache/index";
+    }
+
+
+    @PostMapping("/filterByTag")                        // Get mapping to "/filterByTag" path.
+    public String filter(@RequestParam String tagNameToFind, Map<String, Object> model) {
+        Iterable<Message> messages;                                     // Variable where we are store messages.
+
+        if (tagNameToFind != null && !tagNameToFind.isEmpty())          // Condition on not void of the parameter.
+            messages = messageRepository.findByTag(tagNameToFind);      // Finding all messages with given tag name.
+        else
+            messages = messageRepository.findAll();                     // On void of the parameter return all messages.
+
+        model.put("messages", messages);                                // Updating of the model.
 
 
         return "mustache/index";
