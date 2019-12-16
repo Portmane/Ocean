c95d637fd13e248ddef654f85075ab4bcb3ed030
Alex <mr.portmane@gmail.com>
2019-12-16 13:57:43 -0800

    User.java v - 0.0.2 <<- Comment: "COMMENT !!!", Module: "Sweater_sender" ->>

--- Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/domain/User.java
+++ Frameworks/Spring/SpringBoot/Sweater_SpringMVCAPP/src/main/java/io/domain/User.java
@@ -1,11 +1,15 @@
 package io.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity                         // Marks that this is databases table template.
 @Table(name = "usr")            // How would table be named. / To what table this entity have to be mapped.
-public class User {
+public class User implements UserDetails {
     @Id                             // Primary key.
     @GeneratedValue(strategy = GenerationType.AUTO) // How would this primary ket be maneged.
     private Long id;                // Variable which stores the ID value.
@@ -61,5 +65,32 @@ public class User {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
     }
 }
