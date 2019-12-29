f7182f0e31793c9f04f7c18dca85209dd611880f
Alex <mr.portmane@gmail.com>
2019-12-29 14:09:27 -0800

    HeroStat.java v - 0.0.1 <<- Comment: "It's only begin.", Module: "ValueObject" ->>

---
+++ JavaDesignPatterns/ValueObject/src/main/java/com/iluwatar/value/object/HeroStat.java
@@ -0,0 +1,19 @@
+package com.iluwatar.value.object;
+
+public class HeroStat {
+    private int strength;               // Are you smart enough to be strength ?
+    private int intelligence;           // Oh you are really smart ?
+    private int luck;                   // Lucky shot.
+
+
+    // Standard constructor.
+    private HeroStat(int strength, int intelligence, int luck) {
+        this.strength = strength;
+        this.intelligence = intelligence;
+        this.luck = luck;
+    }
+//    // Return object of this guy.
+//    public HeroStat valueOf(int strength, int intelligence, int luck) {
+//
+//    }
+}
