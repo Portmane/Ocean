6b919d9b9aaecfd6472467efd8b0417e36b85552
Alex <mr.portmane@gmail.com>
2020-01-04 14:33:56 -0800

    Circle.java v - 0.0.1 <<- Comment: "---", Module: "Visitor" ->>

---
+++ JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/Circle.java
@@ -0,0 +1,21 @@
+package com.realizationWithTwoInterfaces.shapes;
+
+import com.realizationWithTwoInterfaces.visitor.Visitor;
+
+public class Circle extends Dot {
+    private int radius;
+
+    public Circle(int id, int x, int y, int radius) {
+        super(id, x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitCircle(this);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
