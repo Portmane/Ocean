e79ae2e82c48dbdbab615063ccca15824447b3ce
Alex <mr.portmane@gmail.com>
2020-01-05 13:58:39 -0800

    CompoundShape.java v - 0.0.2 <<- Comment: "CompoundShape realization.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/CompoundShape.java
+++ JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/CompoundShape.java
@@ -6,33 +6,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CompoundShape implements Shape {
-    public int id;
-    public List<Shape> children = new ArrayList<>();
+    public int id;                                          // ID.
+    public List<Shape> children = new ArrayList<>();        // List of all compound shapes.
 
-    public CompoundShape(int id) {
+
+    public CompoundShape(int id) {                          // Fully parameterized constructor.
         this.id = id;
     }
 
+
+
     @Override
-    public void move(int x, int y) {
+    public void move(int x, int y) {                        // Move method realization.
         // move shape
     }
 
     @Override
-    public void draw() {
+    public void draw() {                                    // Draw method realization.
         // draw shape
     }
 
-    public int getId() {
-        return id;
-    }
-
     @Override
-    public String accept(Visitor visitor) {
-        return visitor.visitCompoundGraphic(this);
+    public String accept(Visitor visitor) {                 /* Realization of Visitor pattern method. Which gives an ability to
+                                                            * work with object through third part class.*/
+        return visitor.visitCompoundGraphic(this);      /* Gives Visitor class explanation which method it has to call in Collection
+                                                            * of multiple Shapes. */
     }
 
-    public void add(Shape shape) {
+    public void add(Shape shape) {                          // Adds new compound shape.
         children.add(shape);
     }
+
+
+
+    public int getId() {
+        return id;
+    }                       // Getter.
 }
