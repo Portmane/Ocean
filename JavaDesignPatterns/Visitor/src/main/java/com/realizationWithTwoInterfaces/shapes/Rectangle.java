2a4c3da4980bf412a794663dd0ff0966302822f7
Alex <mr.portmane@gmail.com>
2020-01-05 13:57:42 -0800

    Rectangle.java v - 0.0.2 <<- Comment: "Rectangle realization.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/Rectangle.java
+++ JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/Rectangle.java
@@ -3,13 +3,14 @@ package com.realizationWithTwoInterfaces.shapes;
 import com.realizationWithTwoInterfaces.visitor.Visitor;
 
 public class Rectangle implements Shape {
-    private int id;
-    private int x;
-    private int y;
-    private int width;
-    private int height;
+    private int id;                 // ID.
+    private int x;                  // X position.
+    private int y;                  // Y position.
+    private int width;              // Width of the rectangle.
+    private int height;             // Height of the rectangle.
 
-    public Rectangle(int id, int x, int y, int width, int height) {
+
+    public Rectangle(int id, int x, int y, int width, int height) {     // Fully parameterized constructor.
         this.id = id;
         this.x = x;
         this.y = y;
@@ -17,22 +18,28 @@ public class Rectangle implements Shape {
         this.height = height;
     }
 
-    @Override
-    public String accept(Visitor visitor) {
-        return visitor.visitRectangle(this);
-    }
+
 
     @Override
-    public void move(int x, int y) {
+    public void move(int x, int y) {                                    // Move method realization.
         // move shape
     }
 
     @Override
-    public void draw() {
+    public void draw() {                                                // Draw method realization.
         // draw shape
     }
 
-    public int getId() {
+    @Override
+    public String accept(Visitor visitor) {                             /* Realization of Visitor pattern method. Which gives an ability to
+                                                                        * work with object through third part class.*/
+        return visitor.visitRectangle(this);                            /* Gives Visitor class explanation which method it has to call in Collection
+                                                                        * of multiple Shapes. */
+    }
+
+
+
+    public int getId() {                                                // Getters.
         return id;
     }
 
