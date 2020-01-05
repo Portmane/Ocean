52345551aff1e2d076ee634312d696aa6c734ea7
Alex <mr.portmane@gmail.com>
2020-01-05 13:58:00 -0800

    Dot.java v - 0.0.2 <<- Comment: "Dot realization.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/Dot.java
+++ JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/Dot.java
@@ -2,35 +2,42 @@ package com.realizationWithTwoInterfaces.shapes;
 
 import com.realizationWithTwoInterfaces.visitor.Visitor;
 
-public class Dot implements Shape {
-    private int id;
-    private int x;
-    private int y;
+public class Dot implements Shape {                 // Simple figure in our programme.
+    private int id;     // Id of the figure.
+    private int x;      // X position.
+    private int y;      // Y position.
 
-    public Dot() {
+
+    public Dot() {      // Standard constructor.
     }
 
-    public Dot(int id, int x, int y) {
+    public Dot(int id, int x, int y) {      // Fully parameterized constructor.
         this.id = id;
         this.x = x;
         this.y = y;
     }
 
+
+
     @Override
-    public void move(int x, int y) {
-        // move shape
+    public void move(int x, int y) {        // Move method realization.
+        // Move shape
     }
 
     @Override
-    public void draw() {
+    public void draw() {                    // Draw method realization.
         // draw shape
     }
 
-    public String accept(Visitor visitor) {
-        return visitor.visitDot(this);
+    public String accept(Visitor visitor) { /* Realization of Visitor pattern method. Which gives an ability to
+                                            * work with object through third part class.*/
+        return visitor.visitDot(this);      /* Gives Visitor class explanation which method it has to call in Collection
+                                            * of multiple Shapes. */
     }
 
-    public int getX() {
+
+
+    public int getX() {                     // Getters.
         return x;
     }
 
