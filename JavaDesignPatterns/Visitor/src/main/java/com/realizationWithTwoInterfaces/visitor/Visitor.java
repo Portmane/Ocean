d8c9e3f8f060674de22a0653f6df1180fe6c0f24
Alex <mr.portmane@gmail.com>
2020-01-05 14:02:37 -0800

    Visitor.java v - 0.0.2 <<- Comment: "Interface for realization by visit classes.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/visitor/Visitor.java
+++ JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/visitor/Visitor.java
@@ -5,12 +5,12 @@ import com.realizationWithTwoInterfaces.shapes.CompoundShape;
 import com.realizationWithTwoInterfaces.shapes.Dot;
 import com.realizationWithTwoInterfaces.shapes.Rectangle;
 
-public interface Visitor {
-    String visitDot(Dot dot);
+public interface Visitor {                              // Interface for visitor classes realization.
+    String visitDot(Dot dot);                           // Gives access to Dot instance.
 
-    String visitCircle(Circle circle);
+    String visitCircle(Circle circle);                  // Gives access to Circle instance.
 
-    String visitRectangle(Rectangle rectangle);
+    String visitRectangle(Rectangle rectangle);         // Gives access to Rectangle instance.
 
-    String visitCompoundGraphic(CompoundShape cg);
+    String visitCompoundGraphic(CompoundShape cg);      // Gives access to CompoundShape instance.
 }
