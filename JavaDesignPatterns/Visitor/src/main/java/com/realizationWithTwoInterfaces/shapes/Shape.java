5b2fd30057e4f04eff64e6ccd949141a82070d30
Alex <mr.portmane@gmail.com>
2020-01-05 13:54:56 -0800

    Shape.java v - 0.0.2 <<- Comment: "Interface for figures.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/Shape.java
+++ JavaDesignPatterns/Visitor/src/main/java/com/realizationWithTwoInterfaces/shapes/Shape.java
@@ -2,8 +2,11 @@ package com.realizationWithTwoInterfaces.shapes;
 
 import com.realizationWithTwoInterfaces.visitor.Visitor;
 
-public interface Shape {
-    void move(int x, int y);
-    void draw();
-    String accept(Visitor visitor);
+public interface Shape {                    // Interface of all figures.
+    void move(int x, int y);                // Move to X and Y values.
+    void draw();                            // Representation of the figure.
+    String accept(Visitor visitor);         /* Gives us an ability to access the figure and work with it through third
+                                            * part class(Visitor). */
+                                            // Method of the Visitor design pattern.
+
 }
