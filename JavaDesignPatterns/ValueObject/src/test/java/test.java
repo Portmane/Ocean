b81ce087da16a1506d604dd5930acdfa5a90f01b
Alex <mr.portmane@gmail.com>
2020-01-06 13:20:32 -0800

    DEMO_ValueObject.java v - 0.1.1 <<- Comment: "Main example of the module. Here I am demonstrate how this(Value Object) Java pattern can be used and how you can use it.", Module: "ValueObject" ->>

--- JavaDesignPatterns/ValueObject/src/test/java/test.java
+++
@@ -1,8 +0,0 @@
-import com.iluwatar.value.object.HeroStat;
-
-public class test {
-        public static void main(String[] args) {
-            var heroStatInstance = HeroStat.valueOf(10, 1, 8);       // Creating our instance using factory.
-            System.out.println(heroStatInstance.toString());                                // Information about instance.
-        }
-}
