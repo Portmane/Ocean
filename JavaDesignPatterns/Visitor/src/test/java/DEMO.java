f8510fb5faf07c47df1e8e6ccf39d519651bc6b9
Alex <mr.portmane@gmail.com>
2020-01-06 11:00:07 -0800

    DEMO.java v - 0.0.2 <<- Comment: "Main example of the module. Here I am demonstrate how this(Visitor) Java pattern can be used and how you can use it.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/test/java/DEMO.java
+++ JavaDesignPatterns/Visitor/src/test/java/DEMO.java
@@ -3,24 +3,40 @@ import com.realizationWithTwoInterfaces.visitor.XMLExportVisitor;
 
 public class DEMO {
     public static void main(String[] args) {
-        Dot dot = new Dot(1, 10, 55);
-        Circle circle = new Circle(2, 23, 15, 10);
-        Rectangle rectangle = new Rectangle(3, 10, 17, 20, 30);
 
-        CompoundShape compoundShape = new CompoundShape(4);
-        compoundShape.add(dot);
-        compoundShape.add(circle);
-        compoundShape.add(rectangle);
-
-        CompoundShape c = new CompoundShape(5);
-        c.add(dot);
-        compoundShape.add(c);
-
-        export(circle, compoundShape);
+        realizationWithTwoInterfaces.execute();     /* Stats execution of the realization of Visitor Java pattern
+                                                    * allocated in realizationWithTwoInterfaces package.*/
     }
 
-    private static void export(Shape... shapes) {
-        XMLExportVisitor exportVisitor = new XMLExportVisitor();
-        System.out.println(exportVisitor.export(shapes));
+
+
+    static class iluwatar {
+        public static void execute() {
+
+        }
+    }
+
+    static class realizationWithTwoInterfaces {
+        public static void execute() {
+            Dot dot = new Dot(1, 10, 55);               // Creating Dot instance.
+            Circle circle = new Circle(2, 23, 15, 10);  // Creating Circle instance.
+            Rectangle rectangle = new Rectangle(3, 10, 17, 20, 30); // Creating Rectangle instance.
+
+            CompoundShape compoundShape = new CompoundShape(4); // Creating CompoundShape instance.
+            compoundShape.add(dot);         // Adding Dot instance.
+            compoundShape.add(circle);      // ...
+            compoundShape.add(rectangle);   // ...
+
+            CompoundShape childCompoundShape = new CompoundShape(5); // New instance of CompoundShape.
+            childCompoundShape.add(dot);     // Adds Shape of type Dot.
+            compoundShape.add(childCompoundShape);      // Adds CompoundShape instance in List of another CompoundShape.
+
+            export(circle, compoundShape);              // Runs a programme.
+        }
+
+        private static void export(Shape... shapes) {
+            XMLExportVisitor exportVisitor = new XMLExportVisitor();        // Creating one of Visitor classes.
+            System.out.print(exportVisitor.export(shapes));                 // Starts Visiting instances which are given as arguments.
+        }
     }
 }
