333a2f36a923b266ad4c5894108ad1c53cecd315
Alex <mr.portmane@gmail.com>
2020-01-12 13:48:29 -0800

    CommanderVisitor.java v - 0.0.1 <<- Comment: "Visitor for Commander instances.", Module: "Visitor" ->>

---
+++ JavaDesignPatterns/Visitor/src/main/java/com/iluwatar/visitors/CommanderVisitor.java
@@ -0,0 +1,37 @@
+package com.iluwatar.visitors;
+
+import com.iluwatar.units.Commander;
+import com.iluwatar.units.Sergeant;
+import com.iluwatar.units.Soldier;
+
+public class CommanderVisitor implements UnitVisitor{
+    CommanderVisitor() {                    /* Standard constructor. */
+
+    }
+
+
+
+    @Override
+    public void visitSoldier(Soldier soldier) {
+        System.out.println("Sorry, but I don't " +
+                "have permission to work with Soldier.");   /* Implementation for Soldier instance
+                                                             * if it will be send to this UnitVisitor
+                                                             * implementation.*/
+    }
+
+    @Override
+    public void visitSergeant(Sergeant sergeant) {
+        System.out.println("Sorry, but I don't " +
+                "have permission to work with Sergeant.");  /* Implementation for Sergeant instance
+                                                             * if it will be send to this UnitVisitor
+                                                             * implementation.*/
+    }
+
+    @Override
+    public void visitCommander(Commander commander) {
+        System.out.println("I have visited Commander instance with " +
+                "no changes are made in source code, except " +
+                "of accept() method.");                     /* In this graph you can use META DATA of
+                                                            * argument in any way you want to.*/
+    }
+}
