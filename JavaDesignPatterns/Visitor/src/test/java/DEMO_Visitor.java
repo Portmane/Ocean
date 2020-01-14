0b0f104b555610b35c206c56e1a68f31feaa0b8b
Alex <mr.portmane@gmail.com>
2020-01-14 10:34:55 -0800

    DEMO_Visitor.java v - 0.1.2 <<- Comment: "Main example of the module. Here I am demonstrate how this(Visitor) Java pattern can be used and how you can use it.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/test/java/DEMO_Visitor.java
+++ JavaDesignPatterns/Visitor/src/test/java/DEMO_Visitor.java
@@ -1,18 +1,55 @@
+import com.iluwatar.units.Commander;
+import com.iluwatar.units.Sergeant;
+import com.iluwatar.units.Soldier;
+import com.iluwatar.units.Unit;
+import com.iluwatar.visitors.CommanderVisitor;
+import com.iluwatar.visitors.SergeantVisitor;
+import com.iluwatar.visitors.SoldierVisitor;
+import com.iluwatar.visitors.UnitVisitor;
 import com.realizationWithTwoInterfaces.shapes.*;
 import com.realizationWithTwoInterfaces.visitor.XMLExportVisitor;
 
 public class DEMO_Visitor {
     public static void main(String[] args) {
 
-        realizationWithTwoInterfaces.execute();     /* Stats execution of the realization of Visitor Java pattern
+        realizationWithTwoInterfaces.execute();     /* Stats execution of the realization of the Visitor Java pattern
                                                     * allocated in realizationWithTwoInterfaces package.*/
+        iluwatar.execute();                         /* Starts execution of the realization of the Visitor Java pattern
+                                                    * allocated in iluwatar package.*/
     }
 
 
 
     static class iluwatar {
         public static void execute() {
+            Unit unit = new Unit();                             // Creating Unit instance.
+            Commander commander = new Commander();              // Creating Commander instance.
+            Sergeant sergeant = new Sergeant();                 // Creating Sergeant instance.
+            Soldier soldier = new Soldier();                    // Creating Soldier instance.
 
+            Unit[] arrayOfUnits = {unit, commander, sergeant, soldier};       // Putting all our instances in one array.
+            export(arrayOfUnits);
+        }
+
+        private static void export(Unit[]... units) {             // Starts visiting our array of instances.
+
+            UnitVisitor[] arrayOfUnitVisitors = {new CommanderVisitor(),
+                    new SergeantVisitor(),
+                    new SoldierVisitor()};                          // Creating all available Visitors.
+
+
+            for (Unit[] unitArray : units) {                        // Gets access to the array of Units.
+                int whichVisitorIsExecuting = 0;                            /* Variable, which give information, which
+                                                                            * Visitor is executing from arrayOfUnitVisitors array. */
+                for (Unit unit : unitArray) {                               // Gets one of Units.
+                    System.out.println("Visitor NUMBER: " + whichVisitorIsExecuting);
+                    whichVisitorIsExecuting++;
+                    for (UnitVisitor unitVisitor : arrayOfUnitVisitors) {   // Gets one of Visitors.
+                        unit.accept(unitVisitor);                           /* Making visit to the Unit instance using one of
+                                                                            * Visitors.*/
+                    }
+                }
+            }
         }
     }
 
@@ -28,7 +65,7 @@ public class DEMO_Visitor {
             compoundShape.add(rectangle);   // ...
 
             CompoundShape childCompoundShape = new CompoundShape(5); // New instance of CompoundShape.
-            childCompoundShape.add(dot);     // Adds Shape of type Dot.
+            childCompoundShape.add(dot);                 // Adds Shape of type Dot.
             compoundShape.add(childCompoundShape);      // Adds CompoundShape instance in List of another CompoundShape.
 
             export(circle, compoundShape);              // Runs a programme.
