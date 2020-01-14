f3bf44f56eb39e2da4dfbf86115fecfd52a7a440
Alex <mr.portmane@gmail.com>
2020-01-14 10:38:09 -0800

    Unit.java v - 0.0.2 <<- Comment: "Parent class of all Units.", Module: "Visitor" ->>

--- JavaDesignPatterns/Visitor/src/main/java/com/iluwatar/units/Unit.java
+++ JavaDesignPatterns/Visitor/src/main/java/com/iluwatar/units/Unit.java
@@ -3,16 +3,16 @@ package com.iluwatar.units;
 import com.iluwatar.visitors.UnitVisitor;
 
 public class Unit {
-    Unit() {                                    // Standard constructor.
+    public Unit() {                                    // Standard constructor.
 
     }
-    Unit(Unit[] units) {
+    public Unit(Unit[] units) {
 
     }
 
 
 
-    void accept(UnitVisitor unitVisitor) {      /* What will be done if visit will be
+    public void accept(UnitVisitor unitVisitor) {      /* What will be done if visit will be
                                                 * done to Unit instance. */
         System.out.println("Unit instance.");
     }
