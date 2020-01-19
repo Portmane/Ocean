f9ceef16b0e68959d5feaee24628a82ebdc377cc
Alex <mr.portmane@gmail.com>
2020-01-19 13:43:14 -0800

    Shield.java v - 0.0.1 <<- Comment: "Start file for the APP.", Module: "Shield" ->>

---
+++ Frameworks/Spring/SpringBoot/Shield_SpringSecurity/src/main/java/com/Shield.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shield {
+    public static void main(String[] args) {
+        SpringApplication.run(Shield.class, args);      // APP starter.
+    }
+}
