b7b4480630fb67391eb67820e7ed502e2c74fd23
Alex <mr.portmane@gmail.com>
2020-01-23 14:25:22 -0800

    SignInController.java v - 0.1.2 <<- Comment: "NEW API'S.", Module: "Shield" ->>

---
+++ Frameworks/Spring/SpringBoot/Shield_SpringSecurity/src/main/java/com/controllers/SignInController.java
@@ -0,0 +1,24 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController                                 // HOME controller for Shield module.
+public class SignInController {
+    @GetMapping("/")                            // On any GET request from the user, call this method.
+    public String indexPage() {
+        return "index";             // Data of return.
+    }
+
+    @GetMapping("/user")                        // On "/user" GET request.
+    public String userPage() {
+        return "<h1>USER</h1>";     // DATA of return.
+    }
+
+    @GetMapping("/admin")                       // On "/admin" GET request.
+    public String adminPage() {
+        return "<h1>ADMIN</h1>";     // DATA of return.
+    }
+}
